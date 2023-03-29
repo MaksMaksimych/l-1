da4ebab23629b2701d10741fae14e3e08845b17a
MaksMaksimych <99993879+MaksMaksimych@users.noreply.github.com>
2023-03-29 09:50:38 +0400

    Add files via upload

---
+++ саня/MvcApplication369/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PartyInvites.Models;
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse x)
+        {
+            switch (x.Operation)
+            {
+                case "+":
+                    x.Result = (sbyte)(x.Operand1 + x.Operand2);
+                    break;
+                case "-":
+                    x.Result = (sbyte)(x.Operand1 - x.Operand2);
+                    break;
+                case "*":
+                    x.Result = (sbyte)(x.Operand1 * x.Operand2);
+                    break;
+                case "/":
+                    x.Result = (sbyte)(x.Operand1 / x.Operand2);
+                    break;
+            }
+            ViewBag.Result = 24;
+            return View("Thanks", x);
+        }
+    }
+}
---
+++ саня/MvcApplication369/Models/GuestResponse.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PartyInvites.Models
+{
+    public class GuestResponse
+    {
+        public sbyte Operand1 { get; set; }
+        public sbyte Operand2 { get; set; }
+        public string Operation { get; set; }
+        public decimal Result { get; set; }
+    }
+}
